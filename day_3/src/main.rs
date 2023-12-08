56edcd5dfb8ef67a15f8cac4bdfdaf6736be9c65
hoyby <54535198+Hoyby@users.noreply.github.com>
2023-12-08 23:28:13 +0000

    day_3

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,8 +1,90 @@
 
 // Day 3: Gear Ratios
 
+use std::{collections::HashSet, usize};
+
+enum Direction{
+    Lt,
+    Rt,
+    Up,
+    Dn,
+    At,
+}
+
+impl Direction {
+    fn delta(&self, i: usize, n: usize) -> Option<usize> {
+        match self {
+            Direction::Lt | Direction::Up => i.checked_sub(1),
+            Direction::Rt | Direction::Dn => {
+                if i < n - 1 {
+                    Some(i + 1)
+                } else {
+                    None
+                }
+            }
+            _ => Some(i),
+        }
+    }
+}
+
+
+
 fn p1(input: &str) -> i32 {
-    0
+    let matrix = input
+        .lines()
+        .into_iter()
+        .map(|s| s.bytes().collect())
+        .collect::<Vec<Vec<_>>>();
+
+    let (rows, columns) = (matrix.len(), matrix[0].len());
+
+    let mut result = 0;
+    let mut seen = HashSet::new();
+
+    
+
+    for row in 0..rows {
+        for col in 0..columns {
+            if matrix[row][col] != b'.' && (matrix[row][col] < b'0' || matrix[row][col] > b'9') {
+                for (dir_row, dir_col) in [
+                    (Direction::Up, Direction::Lt),
+                    (Direction::Up, Direction::At),
+                    (Direction::Up, Direction::Rt),
+                    (Direction::At, Direction::Lt),
+                    (Direction::At, Direction::At),
+                    (Direction::At, Direction::Rt),
+                    (Direction::Dn, Direction::Lt),
+                    (Direction::Dn, Direction::At),
+                    (Direction::Dn, Direction::Rt),
+                ] {
+                    if let (Some(new_row), Some(new_col)) = (
+                        dir_row.delta(row, rows),
+                        dir_col.delta(col, columns),
+                    ) {
+                        if matrix[new_row][new_col].is_ascii_digit() {
+                            let (number, digit_index_start, digit_index_end) = {
+                                let mut index_start = new_col;
+                                while index_start > 0 && matrix[new_row][index_start - 1].is_ascii_digit() {
+                                    index_start -= 1;
+                                }
+                                let mut index_end = new_col;
+                                while index_end < matrix[new_row].len() && matrix[new_row][index_end].is_ascii_digit() {
+                                    index_end += 1;
+                                }
+                                let number = std::str::from_utf8(&&matrix[new_row][index_start..index_end]).unwrap().parse::<i32>().unwrap();
+                                (number, index_start, index_end)
+                            };
+
+                            if seen.insert((new_row, digit_index_start, digit_index_end)) {
+                                result += number;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    result
 }
 
 
@@ -11,7 +93,7 @@ fn p2(input: &str) -> i32 {
 }
 
 fn main() {
-let input = include_str!("../example_2.txt");
+let input = include_str!("../input.txt");
 println!("Part 1: {}", p1(input));
 println!("Part 2: {}", p2(input));
 }
@@ -23,7 +105,7 @@ use super::*;
 #[test]
 fn test_p1() {
     let input = include_str!("../example_1.txt");
-    assert_eq!(p1(input), 13);
+    assert_eq!(p1(input), 4361);
 }
 
 #[test]
