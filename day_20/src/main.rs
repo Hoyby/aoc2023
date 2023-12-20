93a9fc46c48c1944d06086139ce04fd883f77d35
hoyby <54535198+Hoyby@users.noreply.github.com>
2023-12-20 15:04:31 +0000

    init day_19 & day_20

---
+++ day_20/src/main.rs
@@ -0,0 +1,33 @@
+// Day 20: Pulse Propagation
+
+fn p1(input: &str) -> usize {
+    0
+    }
+    
+    fn p2(input: &str) -> usize {
+        0
+    }
+    
+    fn main() {
+        let input = include_str!("../input.txt");
+        println!("Part 1: {}", p1(input));
+        println!("Part 2: {}", p2(input));
+    }
+    
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+    
+        #[test]
+        fn test_p1() {
+            let input = include_str!("../example_1.txt");
+            assert_eq!(p1(input), 102);
+        }
+    
+        #[test]
+        fn test_p2() {
+            let input = include_str!("../example_2.txt");
+            assert_eq!(p2(input), 1);
+        }
+    }
+    
